97ded62fcd18fb4cbac0a1eafa54ed50fa18a7d9
Phill <phillrucci@hotmail.co.uk>
2026-02-16 17:16:34 +0000

    Create script to generate protobufs.

--- scripts/python_gen.sh
+++ scripts/python_gen.sh
@@ -1,1 +1,37 @@
 #!/usr/bin/env bash
+set -eu
+
+# Values
+VENV_DIR="build/.venv"
+VERSION="${1:-v0}"
+
+
+# Create Virtual Environment
+if ! python3 -c "import ensurepip" >/dev/null 2>&1; then
+  echo "Python virtual environment support is missing."
+
+  echo "On Debian/Ubuntu run:"
+  echo "  sudo apt install python${PY_VER}-venv"
+  exit 1
+fi
+python3 -m venv "$VENV_DIR"
+source "$VENV_DIR/bin/activate"
+pip install --upgrade pip
+pip install protobuf grpcio-tools
+
+
+# Generate protobufs
+readarray -d '' PROTO_FILES < <(find passmanager -type f -name "*.proto" -path "*/${VERSION}/*" -print0)
+
+if [ ${#PROTO_FILES[@]} -eq 0 ]; then
+    echo "No proto files found for version ${VERSION}"
+    deactivate
+    rm -rf "$VENV_DIR"
+    exit 1
+fi
+
+python -m grpc_tools.protoc \
+  -I=. \
+  --python_out=. \
+  --grpc_python_out=. \
+  "${PROTO_FILES[@]}"
